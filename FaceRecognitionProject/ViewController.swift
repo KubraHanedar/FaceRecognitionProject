4de2b6c3a4eae07d1836a376781164b21fc2bc85
Kübra Hanedar <the.hanedar@gmail.com>
2023-01-15 20:31:04 +0300

    kutuphane olusturuldu

---
+++ FaceRecognitionProject/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  FaceRecognitionProject
+//
+//  Created by Kübra Hanedar on 15.01.2023.
+//
+
+import UIKit
+import LocalAuthentication
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    
+    @IBAction func singInClicked(_ sender: Any) {
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+}
+
